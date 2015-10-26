67561ab993ae768e9554103f743203353369538a
Tom Friedhof <tom@activelamp.com>
2015-10-26 16:16:31 -0700

    circle back and check lead phone number

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ function BaseCRM(token) {
   this.token = token;
 }
 
-BaseCRM.prototype.findByPhone = function(number, type) {
+function makeRequest(number, type, token) {
   var d = deferred();
   var req = request.get('https://api.getbase.com/v2/' + type + '?mobile=' + number, {
     'headers': {
-      'Authorization': 'Bearer ' + this.token,
+      'Authorization': 'Bearer ' + token,
       'User-Agent': 'Mozilla/5.0',
       'Accept': 'application/json'
     }
   }, 
   function(err, res, body) {
-    if (d) return err ? d.reject(err) : d.resolve({res: res, body: body});
-    return null;
+    if (JSON.parse(body).items.length < 1) {
+      var lead = makeRequest(number, 'leads', token);
+      lead(function(value) {
+        d.resolve({res: value.res, body: value.body});
+      });
+    }
+    else {
+      if (d) return err ? d.reject(err) : d.resolve({res: res, body: body});
+    }
+    
   });
-
   return d ? d.promise : req;
+}
+
+BaseCRM.prototype.findByPhone = function (number, type) {
+  return makeRequest(number, type, this.token);
 }
 
 module.exports = BaseCRM;
